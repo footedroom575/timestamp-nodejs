60ab4e0c1d61c93b00a3d3af17e2af880f5e1a62
fiction <footedroom575@gmail.com>
2022-08-08 20:26:43 +0500

    added/removed content

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // index.js
-// where your node app starts
 
 // init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// enabled CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -17,13 +15,6 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
 
 
 // listen for requests :)
